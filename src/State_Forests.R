88fbcd244bdd0904e993bf02a2ebfc8354d7fe95
Wang Zilong <zwang937@gatech.edu>
2022-05-20 22:21:25 -0400

    Update State_Forests.R

--- src/State_Forests.R
+++ src/State_Forests.R
@@ -4,7 +4,7 @@ list.of.packages <- c(list.of.packages, "zoo", "dtw", "foreach", "evaluate","rli
 
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, lib=‘/home/zwang937/local/R_libs’, repos="http://cran.us.r-project.org", dependencies = TRUE, INSTALL_opts = '--no-lock')
+if(length(new.packages)) install.packages(new.packages, lib='/home/zwang937/local/R_libs', repos="http://cran.us.r-project.org", dependencies = TRUE, INSTALL_opts = '--no-lock')
 
 
 lapply(list.of.packages, require, character.only = TRUE)
