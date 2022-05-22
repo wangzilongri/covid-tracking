6ff18ca5f4f9cc7b95679dc1bfbb9ec17a84713b
Wang Zilong <zwang937@gatech.edu>
2022-05-22 00:01:00 -0400

    Update Preprocess_us-counties-states.R
    Rbind 2020, 2021, 2022 to form `county_data`

--- src/Preprocess_us-counties-states.R
+++ src/Preprocess_us-counties-states.R
@@ -17,8 +17,17 @@ lapply(list.of.packages, require, character.only = TRUE)
 
 
 # URL of NYTimes Data
-nyt_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+#nyt_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+nyt_url20 <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2020.csv"
+nyt_url21 <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2021.csv"
+nyt_url22 <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2022.csv"
 
+
+
+county_data20 <- read.csv(nyt_url20)
+county_data21 <- read.csv(nyt_url21)
+county_data22 <- read.csv(nyt_url22)
+county_data <- rbind(rbind(county_data20,county_data21),county_data22)
 
 destfile <- paste("../data/us-counties_latest",".csv",sep="")
 county_data <- read.csv(nyt_url)
